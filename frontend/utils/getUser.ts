e3be0be501f73567d566acf3538a05e663408be5
mono0218 <mono@monokamo.com>
2025-02-07 01:22:48 +0900

    Add: RPC Setup

--- frontend/utils/getUser.ts
+++ frontend/utils/getUser.ts
@@ -3,7 +3,11 @@ import { hc } from 'hono/client'
 const base_url:string = `${process.env.BASE_URL}`;
 const client = hc<AppType>(base_url)
 
-client.user.$get().then(async (result)=>{
+client.user.$get({},{
+    headers: {
+        Authorization: 'Bearer TOKEN',
+    },
+}).then(async (result)=>{
     const json  = await result.json()
     console.log(json.status)
 })
