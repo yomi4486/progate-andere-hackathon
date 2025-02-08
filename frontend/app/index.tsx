ffe179a1040e00c5b998a0a2dabb086492c8d10d
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2025-02-08 19:52:25 +0900

    ログインボタンのUIを修正

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import {
   Button,
   SafeAreaView,
-  View
+  View,
+  Text,
+  TouchableOpacity,
 } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 import { useAuthContext } from '../utils/authContext';
 import 'react-native-reanimated';
 
@@ -11,7 +14,14 @@ export const LoginScreen = () => {
   const { googleSignIn } = useAuthContext();
   return (
     <SafeAreaView>
-        <Button onPress={googleSignIn} title={'Googleログイン'} />
+        <View style={{height:"100%",justifyContent:'center',alignItems:'center',}}>
+        <TouchableOpacity onPress={googleSignIn}>
+            <View style={{justifyContent:'center',alignItems:'center',backgroundColor:'#eeeeee',borderRadius:30,padding:15,flexDirection:'row'}} >
+                <FontAwesome name="google" size={25} color="#222222" style={{paddingRight:10}} />
+                <Text style={{fontSize:20}}>Googleでログイン</Text>
+            </View>
+        </TouchableOpacity>
+        </View>
     </SafeAreaView>
   );
 };
