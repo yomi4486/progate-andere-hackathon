f9acefd2ace52d0c2383f42c29e50e1d8659bd30
mono0218 <mono@monokamo.com>
2025-02-09 12:05:27 +0900

    Add: UserRouteTest

--- backend/src/routes/user/scheme.ts
+++ backend/src/routes/user/scheme.ts
@@ -1,19 +1,17 @@
 import { z } from 'zod'
 
 export const createUserScheme = z.object({
-	id: z.string(),
 	username: z.string(),
-	icon: z.string(),
+	icon_url: z.string(),
 	status: z.string(),
 	status_message: z.string(),
 	introduction: z.string(),
 })
 
 export const updateUserScheme = z.object({
-	id: z.string().optional(),
-	username: z.string().optional(),
-	icon: z.string().optional(),
-	status: z.string().optional(),
-	status_message: z.string().optional(),
-	introduction: z.string().optional(),
+	username: z.string().nullable(),
+	icon_url: z.string().nullable(),
+	status: z.string().nullable(),
+	status_message: z.string().nullable(),
+	introduction: z.string().nullable(),
 })
