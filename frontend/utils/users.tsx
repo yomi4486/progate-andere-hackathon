495bfa525e4a71f0a53c818be49ff51f6f585625
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2025-02-11 21:46:57 +0900

    fix: conflict

---
+++ frontend/utils/users.tsx
@@ -0,0 +1,65 @@
+import { AppType } from "../../backend/src";
+const { hc } = require("hono/dist/client") as typeof import("hono/client");
+import { HonoResponseType } from "./resnposeType";
+
+const base_url: string = `${process.env.EXPO_PUBLIC_BASE_URL}`;
+const client = hc<AppType>(base_url);
+
+type postUserdata = {
+  status: string;
+  username: string;
+  icon_url: string;
+  status_message: string;
+  introduction: string;
+};
+
+// 自分のユーザーを取得
+export async function get(
+  idToken: string | undefined
+): Promise<HonoResponseType<typeof client.users.$get>> {
+  if (idToken) {
+    const result = await client.users.$get(
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+        },
+      }
+    );
+
+    if (result.ok) {
+      const json = await result.json();
+      return json;
+    } else {
+      throw Error("Fetch to Server");
+    }
+  } else {
+    throw Error("idToken is undefind");
+  }
+}
+export async function post(
+  idToken: string | undefined,
+  data: postUserdata
+): Promise<HonoResponseType<typeof client.users.$post> | null> {
+  if (idToken) {
+    try {
+      const res = await client.users.$post(
+        {
+          json: data,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${idToken}`,
+          },
+        }
+      );
+
+      const json = await res.json();
+      return json;
+    } catch (e) {
+      return null;
+    }
+  } else {
+    throw Error("idToken is undefined");
+  }
+}
