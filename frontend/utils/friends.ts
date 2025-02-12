69aa9e3339d43e77542fcb59428d3682e1127f80
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2025-02-12 21:11:51 +0900

    friend.ts内の型を明示的にしました

--- frontend/utils/friends.ts
+++ frontend/utils/friends.ts
@@ -1,11 +1,12 @@
 import { AppType } from "../../backend/src";
+import { HonoRequestType } from "./requestType";
 const { hc } = require("hono/dist/client") as typeof import("hono/client");
 import { HonoResponseType } from "./resnposeType";
 
 const base_url: string = `${process.env.EXPO_PUBLIC_BASE_URL}`;
 const client = hc<AppType>(base_url);
-
-export async function post(idToken: string, id: string) {
+const idFriend =client.friends[":id"];
+export async function post(idToken: string, id: string):Promise<HonoResponseType<typeof idFriend.$post> | null> {
   if (idToken) {
     try {
       const res = await client.friends[":id"].$post(
@@ -31,7 +32,7 @@ export async function post(idToken: string, id: string) {
   }
 }
 
-export async function put(idToken: string, id: string) {
+export async function put(idToken: string, id: string, status:HonoRequestType<typeof idFriend.$put>) {
   if (idToken) {
     try {
       const res = await client.friends[":id"].$put(
@@ -39,7 +40,7 @@ export async function put(idToken: string, id: string) {
           param: {
             id: id,
           },
-          json: { status: "ACCEPTED" },
+          json: status,
         },
         {
           headers: {
