8ad50bb448aa59650fe19f1f6253ed9596da7f07
yomi4486 <yomi4486@users.noreply.github.com>
2025-02-14 08:00:13 +0000

    Apply Prettier Change

--- frontend/app/(tabs)/history/index.tsx
+++ frontend/app/(tabs)/history/index.tsx
@@ -7,7 +7,7 @@ import { localStyles } from "@/app/styles";
 export default function HistoryScreen() {
   return (
     <View>
-      <DefaultHeader title="着信履歴" showSettingButton={true}/>
+      <DefaultHeader title="着信履歴" showSettingButton={true} />
       {/* ここに要素を追加していく */}
       <View style={localStyles.searchContainer}>
         <FontAwesome name="search" size={20} color="#a0a0a0" />
@@ -18,10 +18,34 @@ export default function HistoryScreen() {
         />
       </View>
       <ScrollView>
-        <CallHistoryItem friendName="yomi" callTimestamp="30分前" isOutgoing={false} callDuration={"1:20:10"} isCaller={false} />
-        <CallHistoryItem friendName="mono" callTimestamp="1時間前" isOutgoing={true} callDuration={"不在だから見えないはず"} isCaller={true} />
-        <CallHistoryItem friendName="まる" callTimestamp="1時間前" isOutgoing={true} callDuration={"不在だから見えないはず"} isCaller={false} />
-        <CallHistoryItem friendName="yomi" callTimestamp="23時間前" isOutgoing={false} callDuration={"30:00"} isCaller={true} />
+        <CallHistoryItem
+          friendName="yomi"
+          callTimestamp="30分前"
+          isOutgoing={false}
+          callDuration={"1:20:10"}
+          isCaller={false}
+        />
+        <CallHistoryItem
+          friendName="mono"
+          callTimestamp="1時間前"
+          isOutgoing={true}
+          callDuration={"不在だから見えないはず"}
+          isCaller={true}
+        />
+        <CallHistoryItem
+          friendName="まる"
+          callTimestamp="1時間前"
+          isOutgoing={true}
+          callDuration={"不在だから見えないはず"}
+          isCaller={false}
+        />
+        <CallHistoryItem
+          friendName="yomi"
+          callTimestamp="23時間前"
+          isOutgoing={false}
+          callDuration={"30:00"}
+          isCaller={true}
+        />
       </ScrollView>
     </View>
   );
