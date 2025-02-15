52227353ec509c0abca8645651ec322fcb5f2e69
mono0218 <mono@monokamo.com>
2025-02-15 23:58:28 +0900

    Fix: UserDBReturnFriend

--- frontend/app/components/FriendItem.tsx
+++ frontend/app/components/FriendItem.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { StyleSheet, View, Text } from 'react-native'
 import { FontAwesome } from '@expo/vector-icons'
 import UserIcon from './UserIcon'
+import { usePubSub } from '../../utils/PubSubContext'
+import * as Room from '../../utils/room'
+import { useRouter } from 'expo-router'
+import { useAuth } from '../../utils/authContext'
 
 interface FriendItemProps {
 	id: string
@@ -10,6 +14,16 @@ interface FriendItemProps {
 }
 
 const FriendItem: React.FC<FriendItemProps> = ({ id, name, message }) => {
+	const router = useRouter()
+	const { user } = useAuth()
+	const { sendMessage } = usePubSub()
+
+	const call = async () => {
+		if (!user || !user.data || !user.data.idToken) return
+		const roomId = await Room.postRoomById(user.data.idToken, id)
+		sendMessage(`${id}/call`, JSON.stringify({ roomId }))
+		router.replace(`/call/${roomId}`)
+	}
 	return (
 		<View style={styles.friend}>
 			<View style={styles.friendIconContainer}>
@@ -21,7 +35,13 @@ const FriendItem: React.FC<FriendItemProps> = ({ id, name, message }) => {
 					<Text style={styles.friendMessage}>{message}</Text>
 				</View>
 			</View>
-			<FontAwesome name="phone" style={styles.callIcon} />
+			<FontAwesome
+				name="phone"
+				style={styles.callIcon}
+				onPress={() => {
+					call()
+				}}
+			/>
 		</View>
 	)
 }
