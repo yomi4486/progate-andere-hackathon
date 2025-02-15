df1e2020a98667bb062e23ccca14ad13ad26d96c
mono0218 <mono@monokamo.com>
2025-02-15 13:04:12 +0900

    Fix: PubSub WsConnections

--- broker/src/index.ts
+++ broker/src/index.ts
@@ -1,50 +1,57 @@
 import aedes from "aedes";
-import net from "net";
+import ws from "websocket-stream";
+import httpServer from "http";
 
-const broker = new aedes();
-const server = net.createServer(broker.handle);
-const port = 1883;
-
-// Client error event
-broker.on("clientError", function (client, err) {
-  console.log(
-    "client error",
-    client ? client.id : "unknown",
-    err.message,
-    err.stack,
+try {
+  const broker = new aedes();
+  const httpserver = httpServer.createServer();
+  ws.createServer(
+    { server: httpserver },
+    broker.handle as unknown as () => void,
   );
-});
 
-// Connection error event
-broker.on("connectionError", function (client, err) {
-  console.log(
-    "connection error",
-    client ? client.id : "unknown",
-    err.message,
-    err.stack,
-  );
-});
+  // Client error event
+  broker.on("clientError", function (client, err) {
+    console.log(
+      "client error",
+      client ? client.id : "unknown",
+      err.message,
+      err.stack,
+    );
+  });
 
-// Publish event - triggered when a message is published
-broker.on("publish", function (packet, client) {
-  if (client) {
-    console.log("message from client", client.id, packet.payload.toString());
-  }
-});
+  // Connection error event
+  broker.on("connectionError", function (client, err) {
+    console.log(
+      "connection error",
+      client ? client.id : "unknown",
+      err.message,
+      err.stack,
+    );
+  });
 
-// Subscribe event - triggered when a new client subscribes
-broker.on("subscribe", function (subscriptions, client) {
-  if (client) {
-    console.log("subscribe from client", subscriptions, client.id);
-  }
-});
+  // Publish event - triggered when a message is published
+  broker.on("publish", function (packet, client) {
+    if (client) {
+      console.log("message from client", client.id, packet.payload.toString());
+    }
+  });
 
-// Client event - triggered when a new client connects
-broker.on("client", function (client) {
-  console.log("new client", client.id);
-});
+  // Subscribe event - triggered when a new client subscribes
+  broker.on("subscribe", function (subscriptions, client) {
+    if (client) {
+      console.log("subscribe from client", subscriptions, client.id);
+    }
+  });
 
-// Start the MQTT broker server
-server.listen(port, function () {
-  console.log("server listening on port", port);
-});
+  broker.on("client", function (client) {
+    console.log("new client", client.id);
+  });
+
+  // Start the MQTT broker server
+  httpserver.listen(3000, function () {
+    console.log("server listening on port", 3000);
+  });
+} catch (e) {
+  console.error("Error:", e);
+}
--- frontend/utils/PubSubContext.tsx
+++ frontend/utils/PubSubContext.tsx
@@ -18,40 +18,37 @@ export const PubSubProvider: React.FC<{ children: React.ReactNode }> = ({
   }
 
   const userId = user.data.user.id;
-  const [friends, setFriends] = useState<string[]>([]);
+  const [friends, setFriends] = useState<string[]>(["aaaa", "bbbb"]);
   const [friendsStatus, setFriendsStatus] = useState<Record<string, string>>(
     {},
   );
 
   useEffect(() => {
-    const client = mqtt.connect("mqtt://localhost:1883");
-
-    client.on("connect", () => {
-      console.log("Connected to MQTT");
-
-      // 自分のステータスを送信
-      client.publish(`status/${userId}`, "online", { retain: true });
-
-      // フレンドのステータスを購読
-      friends.forEach((friendId) => {
-        client.subscribe(`status/${friendId}`);
+    try {
+      const client = mqtt.connect("ws://localhost:3000", {
+        rejectUnauthorized: false,
       });
-    });
 
-    client.on("message", (topic, message) => {
-      const [_, friendId] = topic.split("/");
-      setFriendsStatus((prev) => ({
-        ...prev,
-        [friendId]: message.toString(),
-      }));
-    });
+      client.on("connect", () => {
+        client.publish(`status/${userId}`, "online", { retain: true });
 
-    return () => {
-      // クリーンアップ処理: オフライン通知を送信 & クライアントを閉じる
-      client.publish(`status/${userId}`, "offline", { retain: true });
-      client.end();
-    };
-  }, [userId, friends]);
+        // フレンドのステータスを購読
+        friends.forEach((friendId) => {
+          client.subscribe(`status/${friendId}`);
+        });
+      });
+
+      client.on("message", (topic, message) => {
+        const [_, friendId] = topic.split("/");
+        setFriendsStatus((prev) => ({
+          ...prev,
+          [friendId]: message.toString(),
+        }));
+      });
+    } catch (e) {
+      console.error("error:" + e);
+    }
+  }, [friends]);
 
   const setFriendList = (friends: string[]) => {
     setFriends(friends);
