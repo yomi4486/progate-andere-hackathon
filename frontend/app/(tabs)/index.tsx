2b11c300bfae49cb405b06f9b4339d171081ce10
りおちん <118029.ichikama@gmail.com>
2025-02-16 05:11:27 +0900

    integrate code

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,4 +1,11 @@
-import { StyleSheet, TextInput, TouchableOpacity, Image } from 'react-native'
+import {
+	StyleSheet,
+	TextInput,
+	TouchableOpacity,
+	Image,
+	ScrollView,
+	RefreshControl,
+} from 'react-native'
 import { Text, View } from '@/components/Themed'
 import { FontAwesome } from '@expo/vector-icons'
 import FriendItem from '../components/FriendItem' // 追加
@@ -14,6 +21,8 @@ export default function HomeScreen() {
 	const [userData, setUserData] =
 		useState<Awaited<ReturnType<typeof Users.get>>>()
 
+	const [refreshing, setRefreshing] = useState(false)
+	const [refreshNumber, setRefreshNumber] = useState<number>(0)
 	useEffect(() => {
 		;(async () => {
 			if (idToken) {
@@ -21,7 +30,13 @@ export default function HomeScreen() {
 				setUserData(res)
 			}
 		})()
-	}, [])
+	}, [refreshNumber])
+
+	function pushToReload(): void {
+		setRefreshing(true)
+		setRefreshNumber(refreshNumber + 1)
+		setRefreshing(false)
+	}
 
 	return (
 		<View style={{ height: '100%' }}>
@@ -36,99 +51,116 @@ export default function HomeScreen() {
 				icon="add"
 				color="#FFFFFF"
 			/>
-			<View style={profileStyles.profileContainer}>
-				<View>
-					{currentUserInfo ? (
-						<Image
-							source={{ uri: currentUserInfo['icon_url'] }}
-							style={{
-								width: 100,
-								height: 100,
-								borderRadius: 50,
+			<ScrollView
+				refreshControl={
+					<RefreshControl
+						refreshing={refreshing}
+						onRefresh={pushToReload}
+					/>
+				}
+			>
+				<View style={profileStyles.profileContainer}>
+					<View>
+						{currentUserInfo ? (
+							<Image
+								source={{ uri: currentUserInfo['icon_url'] }}
+								style={{
+									width: 100,
+									height: 100,
+									borderRadius: 50,
+								}}
+							/>
+						) : (
+							<FontAwesome
+								name="user-circle"
+								style={styles.Icon}
+							/>
+						)}
+					</View>
+					<Text style={profileStyles.profileName}>
+						{currentUserInfo
+							? currentUserInfo['username']
+							: 'loading...'}
+					</Text>
+					<TouchableOpacity
+						style={profileStyles.profileStatusContainer}
+					>
+						<FontAwesome
+							name="circle"
+							style={profileStyles.activeDot}
+						/>
+						<Text style={profileStyles.profileStatus}>
+							アクティブ
+						</Text>
+					</TouchableOpacity>
+					<View style={profileStyles.statusInputContainer}>
+						<TextInput
+							style={profileStyles.statusInput}
+							placeholder="現在のステータスを入力"
+							defaultValue={
+								currentUserInfo
+									? currentUserInfo['status_message']
+									: ''
+							}
+							onChangeText={(text) => {
+								setStatusMessage(text)
 							}}
 						/>
-					) : (
-						<FontAwesome name="user-circle" style={styles.Icon} />
-					)}
-				</View>
-				<Text style={profileStyles.profileName}>
-					{currentUserInfo
-						? currentUserInfo['username']
-						: 'loading...'}
-				</Text>
-				<TouchableOpacity style={profileStyles.profileStatusContainer}>
-					<FontAwesome
-						name="circle"
-						style={profileStyles.activeDot}
-					/>
-					<Text style={profileStyles.profileStatus}>アクティブ</Text>
-				</TouchableOpacity>
-				<View style={profileStyles.statusInputContainer}>
-					<TextInput
-						style={profileStyles.statusInput}
-						placeholder="現在のステータスを入力"
-						defaultValue={
-							currentUserInfo
-								? currentUserInfo['status_message']
-								: ''
-						}
-						onChangeText={(text) => {
-							setStatusMessage(text)
-						}}
-					/>
-					<FontAwesome
-						name={
-							currentUserInfo &&
-							statusMessage == currentUserInfo['status_message']
-								? 'pencil'
-								: 'check'
-						}
-						style={profileStyles.statusEditIcon}
-						onPress={async () => {
-							const res = await Users.put(
-								{
-									status: undefined,
-									username: undefined,
-									icon_url: undefined,
-									status_message: statusMessage,
-									introduction: undefined,
-								},
-								idToken!,
-							)
-							if (currentUserInfo) {
-								const a: typeof currentUserInfo =
-									currentUserInfo
-								a!.status_message = res!['status_message']
-								updateCurrentUserInfo(a)
-								setStatusMessage(res!['status_message'])
+						<FontAwesome
+							name={
+								currentUserInfo &&
+								statusMessage ==
+									currentUserInfo['status_message']
+									? 'pencil'
+									: 'check'
 							}
-						}}
-					/>
+							style={profileStyles.statusEditIcon}
+							onPress={async () => {
+								const res = await Users.put(
+									{
+										status: undefined,
+										username: undefined,
+										icon_url: undefined,
+										status_message: statusMessage,
+										introduction: undefined,
+									},
+									idToken!,
+								)
+								if (currentUserInfo) {
+									const a: typeof currentUserInfo =
+										currentUserInfo
+									a!.status_message = res!['status_message']
+									updateCurrentUserInfo(a)
+									setStatusMessage(res!['status_message'])
+								}
+							}}
+						/>
+					</View>
 				</View>
-			</View>
-			<View style={profileStyles.friendsContainer}>
-				<Text style={profileStyles.friendsTitle}>
-					アクティブなフレンド
-				</Text>
-				<View style={profileStyles.friendsBox}>
-					{userData ? (
-						userData.friends.map((friend) => {
-							return (
-								<FriendItem
-									key={friend.id}
-									id={friend.id}
-									name={friend.username}
-									message={friend.status_message}
-									icon_url={friend.icon_url}
-								/>
-							)
-						})
-					) : (
-						<Text>loading...</Text>
-					)}
-					{/* 他のフレンドも同様に追加 */}
+				<View style={profileStyles.friendsContainer}>
+					<Text style={profileStyles.friendsTitle}>
+						アクティブなフレンド
+					</Text>
+					<View style={profileStyles.friendsBox}>
+						{userData ? (
+							userData.friends.map((friend) => {
+								return (
+									<FriendItem
+										key={friend.id}
+										id={friend.id}
+										name={friend.username}
+										message={friend.status_message}
+										icon_url={friend.icon_url}
+									/>
+								)
+							})
+						) : (
+							<Text>loading...</Text>
+						)}
+						{/* 他のフレンドも同様に追加 */}
+					</View>
 				</View>
-			</View>
+			</ScrollView>
 		</View>
 	)
 }
